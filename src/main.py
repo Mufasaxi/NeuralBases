261cf7fe62c558fb5f7b532da80051d946fc4a1f
Mustafa <mustafa.abdalla@tu-clausthal.de>
2024-07-21 23:54:58 +0200

    feat: implement basics of minmax with alpha-beta pruning

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from prober import Prober
+from uci_prober_engine import UCIProberEngine
 
 def main() -> None:
-    tablebase_prober = Prober()
+    # tablebase_prober = Prober()
 
-    fen = "8/8/p7/8/p4P2/8/k1P5/5K2 w - - 0 1"
-    tablebase_prober.evaluate_position(fen)
+    # fen = "8/8/p7/8/p4P2/8/k1P5/5K2 w - - 0 1"
+    # tablebase_prober.evaluate_position(fen)
+    
+    engine = UCIProberEngine()
+    engine.uci_loop()
 
 
 if __name__ == "__main__":
