0f54574dd33e93066a02139288caa80133f72eff
Mustafa <mustafalbahar@hotmail.com>
2024-08-13 19:08:49 +0200

    feat: Improve architecture of neural network. Restructure model, dataset, and train files. Train model for 10 epochs, and save weights for prediction

--- src/neuralnet/model.py
+++ src/neuralnet/model.py
@@ -2,20 +2,21 @@ import torch.nn as nn
 
 class EndgameModel(nn.Module):
     
-    def __init__(self, num_classes):
+    def __init__(self):
         super(EndgameModel, self).__init__()
-
         self.conv1 = nn.Conv2d(12, 64, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(8*8*128, 256)
-        self.fc2 = nn.Linear(256, num_classes)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(2,2)
+        self.fc1 = nn.Linear(256 * 1 * 1, 512)
+        self.fc2 = nn.Linear(512, 5)
         self.relu = nn.ReLU()
 
     def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.relu(self.conv2(x))
-        x = self.flatten(x)
+        x = self.pool(self.relu(self.conv1(x)))
+        x = self.pool(self.relu(self.conv2(x)))
+        x = self.pool(self.relu(self.conv3(x)))
+        x = x.view(x.size(0), -1)
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
         return x
