35acf22a9ba83ac285ca1b9d6ff1324c8cdc58ef
Mustafa <mustafa.abdalla@tu-clausthal.de>
2024-08-13 19:08:49 +0200

    feat: Improve architecture of neural network. Restructure model, dataset, and train files. Train model for 10 epochs, and save weights for prediction

--- src/neuralnet/dataset.py
+++ src/neuralnet/dataset.py
@@ -1,13 +1,60 @@
-from torch.utils.data import Dataset
+import numpy as np
+import torch
+from torch.utils.data import Dataset, DataLoader
+import multiprocessing
+from functools import partial
+from tqdm import tqdm
+import time
+from fenGenerator import FenGen
+from fenParser import create_nn_input
+
 
 class EndgamesDataset(Dataset):
 
     def __init__(self, X, y):
-        self.X = X
-        self.y = y
+        self.X = torch.FloatTensor(X)
+        self.y = torch.LongTensor(y+2)
 
     def __len__(self) -> int:
-        return len(self.X)
+        return len(self.y)
     
     def __getitem__(self, index: int) -> tuple[int, int]:
         return self.X[index], self.y[index]
+    
+def process_batch(_, batch_size, fenGen):
+    fens = [fenGen.make_fen() for _ in range(batch_size)]
+    return create_nn_input(fens)
+
+def create_data():
+    total_fens = 1000000
+    batch_size = 1000
+    num_batches = total_fens // batch_size
+
+    # Define file paths for saving/loading
+    x_file = 'X_data.npy'
+    y_file = 'y_data.npy'
+
+    print("Generating new data...")
+    fenGen = FenGen()
+
+    # Create a partial function with the batch size and fenGen
+    process_batch_partial = partial(process_batch, batch_size=batch_size, fenGen=fenGen)
+
+    # Use multiprocessing to parallelize the work
+    with multiprocessing.Pool() as pool:
+        results = list(tqdm(pool.imap(process_batch_partial, range(num_batches)), total=num_batches))
+
+    # Combine the results
+    X = np.concatenate([r[0] for r in results])
+    y = np.concatenate([r[1] for r in results])
+
+    # Save the results
+    print("Saving data...")
+    np.save(x_file, X)
+    np.save(y_file, y)
+
+    
+def load_data(x_path, y_path):
+    X = np.load(x_path)
+    y = np.load(y_path)
+    return X, y
--- src/neuralnet/train.py
+++ src/neuralnet/train.py
@@ -1,107 +1,109 @@
-import multiprocessing
-from functools import partial
-from tqdm import tqdm
-import time
-import numpy as np
-import os
-from fenParser import create_nn_input
-from fenGenerator import FenGen
-
-from dataset import EndgamesDataset
-from model import EndgameModel
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from dataset import EndgamesDataset, load_data, create_data
+from model import EndgameModel
+from fenParser import board_to_matrix
+import chess
+import os
 
-def process_batch(_, batch_size, fenGen):
-    fens = [fenGen.make_fen() for _ in range(batch_size)]
-    return create_nn_input(fens)
+# TODO: Add type hinting and formatting to model train and dataset files
+# TODO: Add option to insert FEN from terminal
 
-def create_np_inputs():
-    total_fens = 1000000
-    batch_size = 1000
-    num_batches = total_fens // batch_size
+def train(model, train_loader, criterion, optimiser, device):
+    model.train()
+    running_loss = 0.0
+    for i , (inputs, labels) in enumerate(train_loader):
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimiser.zero_grad()
+        outputs = model(inputs)
 
-    # Define file paths for saving/loading
-    x_file = 'X_data.npy'
-    y_file = 'y_data.npy'
+        # print(f"Batch {i}: input shape: {inputs.shape}, labels shape: {labels.shape}, outputs shape: {outputs.shape}")
 
-    # Check if saved files already exist
-    if os.path.exists(x_file) and os.path.exists(y_file):
-        print("Loading existing data...")
-        X = np.load(x_file)
-        y = np.load(y_file)
-    else:
-        print("Generating new data...")
-        fenGen = FenGen()
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimiser.step()
+        running_loss += loss.item()
+    return running_loss / len(train_loader)
 
-        # Create a partial function with the batch size and fenGen
-        process_batch_partial = partial(process_batch, batch_size=batch_size, fenGen=fenGen)
+def evaluate(model, val_loader, criterion, device):
+    model.eval()
+    running_loss = 0.0
+    correct = 0
+    total = 0
 
-        # Use multiprocessing to parallelize the work
-        with multiprocessing.Pool() as pool:
-            results = list(tqdm(pool.imap(process_batch_partial, range(num_batches)), total=num_batches))
-
-        # Combine the results
-        X = np.concatenate([r[0] for r in results])
-        y = np.concatenate([r[1] for r in results])
-
-        # Save the results
-        print("Saving data...")
-        np.save(x_file, X)
-        np.save(y_file, y)
-
-    print(f"Total FENs processed: {len(X)}")
-    return X, y
-
-def train(X, y):
-    X = torch.tensor(X, dtype=torch.float32)
-    y = torch.tensor(y, dtype=torch.float32)
-    dataset = EndgamesDataset(X, y)
-    dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "CPU")
-    print(f"Using device: {device}")
-
-    model = EndgameModel(5).to(device)
-    criterion = nn.CrossEntropyLoss()
-    optimiser = optim.Adam(model.parameters(), lr = 0.0001)
-
-
-    epochs = 50
-    for epoch in range(epochs):
-        start_time = time.time()
-        model.train()
-        running_loss = 0.0
-        for inputs, labels in tqdm(dataloader):
-            inputs= inputs.to(device)
-            labels = labels.type(torch.LongTensor)
-            labels = labels.to(device)
-            optimiser.zero_grad()
-
+    with torch.no_grad():
+        for inputs, labels in val_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
             outputs = model(inputs)
-
             loss = criterion(outputs, labels)
-            loss.backward()
-
-            torch.nn.util.clip_grad_norm_(model.parameters(), max_norm=1.0)
-
-            optimiser.step()
             running_loss += loss.item()
-        end_time = time.time()
-        epoch_time = end_time - start_time
-        minutes: int = int(epoch_time // 60)
-        seconds: int = int(epoch_time) - minutes * 60
-        print(f'Epoch {epoch + 1 + 50}/{epochs + 1 + 50}, Loss: {running_loss / len(dataloader):.4f}, Time: {minutes}m{seconds}s')
+            _, predicted = outputs.max(1)
+            total += labels.size(0)
+            correct += predicted.eq(labels).sum().item()
+    accuracy = correct / total
+    return running_loss / len(val_loader), accuracy
 
-        torch.save(model.state_dict(), "../../models/TORCH50EPOCHS.pth")
-        
+def load_model(model_path):
+    model = EndgameModel()
+    state_dict = torch.load(model_path, weights_only=True)
+    model.load_state_dict(state_dict)
+    model.eval()
+    return model
+
+def fen_to_tensor(fen):
+    board_matrix = board_to_matrix(chess.Board(fen))
+    input_tensor = torch.FloatTensor(board_matrix).unsqueeze(0)
+    return input_tensor
+
+def predict(model, input_tensor):
+    with torch.no_grad():
+        output = model(input_tensor)
+        _, predicted = output.max(1)
+    return predicted.item() - 2
+
 
 def main():
-    X, y = create_np_inputs()
-    train(X, y)
+    batch_size = 64
+    learning_rate = 0.001
+    num_epochs = 10
+
+    if os.path.exists("X_data.npy") and os.path.exists("y_data.npy"):
+        X, y = load_data("X_data.npy", "y_data.npy")
+    else: 
+        create_data()
+        X, y = load_data("X_data.npy", "y_data.npy")
+
+    split = int(0.8 * len(y))
+    train_dataset = EndgamesDataset(X[:split], y[:split])
+    val_dataset = EndgamesDataset(X[split:], y[split:])
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = EndgameModel().to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimiser = optim.Adam(model.parameters(), lr = learning_rate)
+
+    for epoch in range(num_epochs):
+        train_loss = train(model, train_loader, criterion, optimiser, device)
+        val_loss, val_accuracy = evaluate(model, val_loader, criterion, device)
+
+        print(f"Epoch {epoch+1}/{num_epochs}:")
+        print(f"Train Loss: {train_loss: .4f}")
+        print(f"Val Loss: {val_loss: .4f}, Val Accuracy: {val_accuracy: .4f}")
+
+    torch.save(model.state_dict(), 'endgame_cnn.pth')
+
 
 
 if __name__ == "__main__":
-    main()
+    model = load_model("endgame_cnn.pth")
+    fen_string = "2K5/4q3/8/3B4/k7/8/8/8 w - - 0 1"
+    input_tensor = fen_to_tensor(fen_string)
+    prediction = predict(model, input_tensor)
+
+    print(f"Input FEN: 2K5/4q3/8/3B4/k7/8/8/8 w - - 0 1")
+    print(f"Predicted WDL: {prediction}")
